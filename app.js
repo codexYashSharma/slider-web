b7c52260b00b2bc08abdd70c07950f379e6f9962
codexYashSharma <yashsharma10405@gmail.com>
2025-02-05 14:57:00 +0530

    commit

--- app.js
+++ app.js
@@ -1,69 +1,42 @@
 let nextButton = document.getElementById('next');
 let prevButton = document.getElementById('prev');
-let backButton = document.getElementById('back');
-let seeMoreButtons = document.querySelectorAll('.seeMore');
 let carousel = document.querySelector('.carousel');
 let listHTML = document.querySelector('.carousel .list');
+let seeMoreButtons = document.querySelectorAll('.seeMore');
+let backButton = document.getElementById('back');
 
-// Next Button Click
-nextButton.onclick = function() {
+nextButton.onclick = function(){
     showSlider('next');
-};
-
-// Previous Button Click
-prevButton.onclick = function() {
+}
+prevButton.onclick = function(){
     showSlider('prev');
-};
-
-// See More Buttons Click
-seeMoreButtons.forEach((button) => {
-    button.onclick = function() {
-        carousel.classList.add('showDetail');
-    };
-});
-
-// Back Button Click
-backButton.onclick = function() {
-    carousel.classList.remove('showDetail');
-};
-
-let unAcceptClick;
-
+}
+let unAcceppClick;
 const showSlider = (type) => {
-    // Disable button clicks during transition
     nextButton.style.pointerEvents = 'none';
     prevButton.style.pointerEvents = 'none';
 
-    // Remove existing animation classes
     carousel.classList.remove('next', 'prev');
-
-    // Get all carousel items
     let items = document.querySelectorAll('.carousel .list .item');
-
-    if (type === 'next') {
-        // Move the first item to the end
+    if(type === 'next'){
         listHTML.appendChild(items[0]);
         carousel.classList.add('next');
-    } else {
-        // Move the last item to the beginning
-        let positionLast = items.length - 1;
-        listHTML.prepend(items[positionLast]);
+    }else{
+        listHTML.prepend(items[items.length - 1]);
         carousel.classList.add('prev');
     }
-
-    // Re-enable button clicks after 2 seconds
-    clearTimeout(unAcceptClick);
-    unAcceptClick = setTimeout(() => {
+    clearTimeout(unAcceppClick);
+    unAcceppClick = setTimeout(()=>{
         nextButton.style.pointerEvents = 'auto';
         prevButton.style.pointerEvents = 'auto';
-    }, 2000);
+    }, 2000)
 }
-
-seeMoreButtons.forEach(button => {
+seeMoreButtons.forEach((button) => {
     button.onclick = function(){
+        carousel.classList.remove('next', 'prev');
         carousel.classList.add('showDetail');
     }
-})
-backButton.oncliclick = function(){
+});
+backButton.onclick = function(){
     carousel.classList.remove('showDetail');
 }
